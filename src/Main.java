34fe95c41879d89f6da5c053fe1b27dd5edf23a0
APalmer23 <avap.palmer@gmail.com>
2026-05-15 08:38:55 -0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println("Welcome to the Student Stress Analyzer!");
+        while (true){
+            StudentAnalysis.console();
+        }
     }
 }
--- src/StudentAnalysis.java
+++ src/StudentAnalysis.java
@@ -2,15 +2,15 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
+
 //id,study_hours,sleep_hours,social_media_hours,exam_score,stress_level
 public class StudentAnalysis {
-    ArrayList<Student> students = new ArrayList<>();
-    public void loadFromFile(File file) throws FileNotFoundException {
+    static ArrayList<Student> students = new ArrayList<>();
+    public static void loadFromFile(File file) throws FileNotFoundException {
         Scanner scanner = new Scanner(file);
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
-            String[] values = line.fsplit(",");
+            String[] values = line.split(",");
             Student student = new Student(
                     Integer.parseInt(values[0]),
                     Integer.parseInt(values[1]),
@@ -22,39 +22,49 @@ public class StudentAnalysis {
             students.add(student);
         }
     }
-    public ArrayList<Student> listByStudyHours(){
-
+    public static ArrayList<Student> listByStudyHours(){
+        ArrayList<Student> arr = students;
+        arr.sort((a,b)->Integer.compare(b.getStudy_hours(),a.getStudy_hours()));
+        return arr;
     }
-    public ArrayList<Student> listBySleepHours(){
-
+    public static ArrayList<Student> listBySleepHours(){
+        ArrayList<Student> arr = students;
+        arr.sort((a,b)->Integer.compare(b.getSleep_hours(),a.getSleep_hours()));
+        return arr;
     }
-    public ArrayList<Student> listByMediaHours(){
-
+    public static ArrayList<Student> listByMediaHours(){
+        ArrayList<Student> arr = students;
+        arr.sort((a,b)->Integer.compare(b.getSocial_media_hours(),a.getSocial_media_hours()));
+        return arr;
     }
-    public ArrayList<Student> listByScore(){
-
+    public static ArrayList<Student> listByScore(){
+        ArrayList<Student> arr = students;
+        arr.sort((a,b)->Integer.compare(b.getExam_score(),a.getExam_score()));
+        return arr;
     }
-    public ArrayList<Student> getByLevel(String str){
+    public static ArrayList<Student> getByLevel(String str){
         ArrayList<Student> arr = new ArrayList<>();
-        if (!str.equals("High")&&!str.equals("Medium")&&!str.equals("Low"))
         for (Student s:students){
-            if 
+            if (s.getStress_level().equals(str)){
+                arr.add(s);
+            }
         }
+        return arr;
     }
-    public Student getStudent(int id){
+    public static Student getStudent(int id){
         return students.get(id-1);
     }
-    public int getMeanSleepHours(){
+    public static int getMeanSleepHours(){
         int sum=0;
         for (Student s:listBySleepHours()){
             sum+=s.getSleep_hours();
         }
         return sum/students.size();
     }
-    public int getMedianSleepHours(){
+    public static int getMedianSleepHours(){
         return(listBySleepHours().get(students.size()/2)).getSleep_hours();
     }
-    public int getModeSleepHours(){
+    public static int getModeSleepHours(){
         int[] log = new int[24];
         int max=0;
         for (Student s:listBySleepHours()){
@@ -65,17 +75,17 @@ public class StudentAnalysis {
         }
         return max;
     }
-    public int getMeanStudyHours(){
+    public static int getMeanStudyHours(){
         int sum=0;
         for (Student s:listByStudyHours()){
             sum+=s.getStudy_hours();
         }
         return sum/students.size();
     }
-    public int getMedianStudyHours(){
+    public static int getMedianStudyHours(){
         return(listByStudyHours().get(students.size()/2)).getStudy_hours();
     }
-    public int getModeStudyHours(){
+    public static int getModeStudyHours(){
         int[] log = new int[24];
         int max=0;
         for (Student s:listByStudyHours()){
@@ -86,17 +96,17 @@ public class StudentAnalysis {
         }
         return max;
     }
-    public int getMeanMediaHours(){
+    public static int getMeanMediaHours(){
         int sum=0;
         for (Student s:listByMediaHours()){
             sum+=s.getSocial_media_hours();
         }
         return sum/students.size();
     }
-    public int getMedianMediaHours(){
+    public static int getMedianMediaHours(){
         return(listByMediaHours().get(students.size()/2)).getSocial_media_hours();
     }
-    public int getModeMediaHours(){
+    public static int getModeMediaHours(){
         int[] log = new int[24];
         int max=0;
         for (Student s:listByMediaHours()){
@@ -107,17 +117,17 @@ public class StudentAnalysis {
         }
         return max;
     }
-    public int getMeanScore(){
+    public static int getMeanScore(){
         int sum=0;
         for (Student s:listByScore()){
             sum+=s.getExam_score();
         }
         return sum/students.size();
     }
-    public int getMedianScore(){
+    public static int getMedianScore(){
         return(listByScore().get(students.size()/2)).getExam_score();
     }
-    public int getModeScore(){
+    public static int getModeScore(){
         int[] log = new int[100];
         int max=0;
         for (Student s:listByScore()){
@@ -127,5 +137,118 @@ public class StudentAnalysis {
             }
         }
         return max;
+    }
+    public static void console() throws FileNotFoundException {
+        int c = 0;
+        Scanner scann = new Scanner(System.in);
+        Scanner scannn = new Scanner(System.in);
+        System.out.println("------------------------------\nData Analyzer\n------------------------------\n");
+        System.out.println("Please select an option:");
+        System.out.println("1. Load student data from file\n2. List students by stat\n3. Get specific student(s)\n4. Get mean, median, or mode\n5. Exit program");
+        System.out.print("Selection: ");
+        c = scann.nextInt();
+        if (c==1){
+            Scanner scan = new Scanner(System.in);
+            String fn=scan.nextLine();
+            File file = new File(fn+=".csv");
+            StudentAnalysis.loadFromFile(file);
+            console();
+        }
+        else if (c ==2){
+            System.out.println("\n1. List by hours spent sleeping\n2. List by hours spent studying\n3. List by hours spent on social media\n4. List by exam score");
+            System.out.print("Selection: ");
+            c = scann.nextInt();
+            if (c==1){
+                System.out.println(listBySleepHours());
+            }
+            else if (c==2){
+                System.out.println(listByStudyHours());
+            }
+            else if (c==3){
+                System.out.println(listByMediaHours());
+            }
+            else if (c==4){
+                System.out.println(listByScore());
+            }
+        }
+        else if (c ==3){
+            System.out.println("\n1. Get specific student\n2. Get students with specific stress level");
+            System.out.print("Selection: ");
+            c=scann.nextInt();
+            if (c==1){
+                System.out.print("Enter student ID: ");
+                c=scann.nextInt();
+                System.out.println(getStudent(c));
+            }
+            else if (c==2){
+                System.out.print("Enter stress level (High, Medium, Low): ");
+                String s = scannn.nextLine();
+                System.out.println(getByLevel(s));
+            }
+        }
+        else if (c ==4){
+            System.out.println("\n1. Get for hours spent sleeping\n2. Get for hours spent studying\n3. Get for hours spent on social media\n4. Get for exam score");
+            System.out.print("Selection: ");
+            c = scann.nextInt();
+            if (c==1){
+                System.out.println("\n1. Get Mean\n2. Get Median\n3. Get Mode");
+                System.out.print("Selection: ");
+                c=scann.nextInt();
+                if (c==1){
+                    System.out.println(getMeanSleepHours());
+                }
+                else if (c==2){
+                    System.out.println(getMedianSleepHours());
+                }
+                else if (c==3){
+                    System.out.println(getModeSleepHours());
+                }
+            }
+            else if (c==2){
+                System.out.println("\n1. Get Mean\n2. Get Median\n3. Get Mode");
+                System.out.print("Selection: ");
+                c=scann.nextInt();
+                if (c==1){
+                    System.out.println(getMeanStudyHours());
+                }
+                else if (c==2){
+                    System.out.println(getMedianStudyHours());
+                }
+                else if (c==3){
+                    System.out.println(getModeStudyHours());
+                }
+            }
+            else if (c==3){
+                System.out.println("\n1. Get Mean\n2. Get Median\n3. Get Mode");
+                System.out.print("Selection: ");
+                c=scann.nextInt();
+                if (c==1){
+                    System.out.println(getMeanMediaHours());
+                }
+                else if (c==2){
+                    System.out.println(getMedianMediaHours());
+                }
+                else if (c==3){
+                    System.out.println(getModeMediaHours());
+                }
+            }
+            else if (c==4){
+                System.out.println("\n1. Get Mean\n2. Get Median\n3. Get Mode");
+                System.out.print("Selection: ");
+                c=scann.nextInt();
+                if (c==1){
+                    System.out.println(getMeanScore());
+                }
+                else if (c==2){
+                    System.out.println(getMedianScore());
+                }
+                else if (c==3){
+                    System.out.println(getModeScore());
+                }
+            }
+        }
+        else{
+            System.exit(0);
+        }
     }
 }
